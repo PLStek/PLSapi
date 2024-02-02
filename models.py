54961839bc85dee1e0550c9eae1687b2d7b44878
Wiqiro <william.imbert@utbm.fr>
2024-02-02 21:45:06 +0100

    fix: nullable, onupdate, ondelete precisions for models

--- models.py
+++ models.py
@@ -6,13 +6,17 @@ from database import Base
 
 class Charbon(Base):
     __tablename__ = "charbon"
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(100))
-    description = Column(String(500))
-    datetime = Column(Integer)
-    course_id = Column(String(4), ForeignKey("course.id"))
-    duration = Column(Integer, nullable=True)
-    replay_link = Column(String(100), nullable=True)
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String(100), nullable=False)
+    description = Column(String(500), nullable=False)
+    datetime = Column(Integer, nullable=False)
+    course_id = Column(
+        String(4),
+        ForeignKey("course.id", onupdate="CASCADE"),
+        nullable=False,
+    )
+    duration = Column(Integer)
+    replay_link = Column(String(100))
 
     actionneurs = relationship("CharbonHost", back_populates="charbon")
     course = relationship("Course", back_populates="charbons")
@@ -20,8 +24,8 @@ class Charbon(Base):
 
 class Course(Base):
     __tablename__ = "course"
-    id = Column(String(4), primary_key=True, index=True)
-    type = Column(Enum("meca", "info", "elec", "math"))
+    id = Column(String(4), primary_key=True, nullable=False)
+    type = Column(Enum("meca", "info", "elec", "math"), nullable=False)
 
     charbons = relationship("Charbon", back_populates="course")
     exercise_topics = relationship("ExerciseTopic", back_populates="course")
@@ -29,8 +33,20 @@ class Course(Base):
 
 class CharbonHost(Base):
     __tablename__ = "charbon_host"
-    charbon_id = Column(Integer, ForeignKey("charbon.id"), primary_key=True, index=True)
-    actionneur_id = Column(Integer, ForeignKey("user.id"), primary_key=True, index=True)
+    charbon_id = Column(
+        Integer,
+        ForeignKey("charbon.id", onupdate="CASCADE", ondelete="CASCADE"),
+        primary_key=True,
+        index=True,
+        nullable=False,
+    )
+    actionneur_id = Column(
+        Integer,
+        ForeignKey("user.id", onupdate="CASCADE", ondelete="CASCADE"),
+        primary_key=True,
+        index=True,
+        nullable=False,
+    )
 
     charbon = relationship("Charbon", back_populates="actionneurs")
     actionneur = relationship("User", back_populates="charbons")
@@ -38,29 +54,31 @@ class CharbonHost(Base):
 
 class User(Base):
     __tablename__ = "user"
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(100))
-    email = Column(String(100))
-    password_hash = Column(String(100))
-    is_actionneur = Column(Boolean, default=False)
-    is_admin = Column(Boolean, default=False)
+    id = Column(Integer, primary_key=True, nullable=False)
+    username = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False)
+    password_hash = Column(String(100), nullable=False)
+    is_actionneur = Column(Boolean, default=False, nullable=False)
+    is_admin = Column(Boolean, default=False, nullable=False)
 
     charbons = relationship("CharbonHost", back_populates="actionneur")
 
 
 class Announcement(Base):
     __tablename__ = "announcement"
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(100))
-    content = Column(String(5000))
-    datetime = Column(Integer)
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String(100), nullable=False)
+    content = Column(String(5000), nullable=False)
+    datetime = Column(Integer, nullable=False)
 
 
 class ExerciseTopic(Base):
     __tablename__ = "exercise_topic"
-    id = Column(Integer, primary_key=True, index=True)
-    topic = Column(String(100))
-    course_id = Column(String(4), ForeignKey("course.id"))
+    id = Column(Integer, primary_key=True, nullable=False)
+    topic = Column(String(100), nullable=False)
+    course_id = Column(
+        String(4), ForeignKey("course.id", onupdate="CASCADE"), nullable=False
+    )
 
     course = relationship("Course", back_populates="exercise_topics")
     exercises = relationship("Exercise", back_populates="topic")
@@ -68,12 +86,16 @@ class ExerciseTopic(Base):
 
 class Exercise(Base):
     __tablename__ = "exercise"
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(100))
-    difficulty = Column(Integer)
-    is_corrected = Column(Boolean)
-    source = Column(String(100))
-    topic_id = Column(Integer, ForeignKey("exercise_topic.id"))
-    content = Column(LargeBinary)
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String(100), nullable=False)
+    difficulty = Column(Integer, nullable=False)
+    is_corrected = Column(Boolean, nullable=False)
+    source = Column(String(100), nullable=False)
+    topic_id = Column(
+        Integer,
+        ForeignKey("exercise_topic.id", onupdate="CASCADE", ondelete="CASCADE"),
+        nullable=False,
+    )
+    content = Column(LargeBinary, nullable=False)
 
     topic = relationship("ExerciseTopic", back_populates="exercises")
